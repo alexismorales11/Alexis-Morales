66501dd8bdcd40b47c652641f1ebf4d80f043709
Alexis Morales <selvinmoralesoficial@gmail.com>
2023-01-07 23:10:54 -0600

    Changes vista cv

--- js/header.js
+++ js/header.js
@@ -2,14 +2,13 @@ const burger = document.querySelector(".container_burger");
 const cross = document.querySelector(".container_cross");
 const menumobile = document.querySelector(".container_header_mobile_general");
 
-
-burger.addEventListener("click", function(){
-    menumobile.classList.add("show");
+burger.addEventListener("click", function () {
+  menumobile.classList.add("show");
 });
 
-cross.addEventListener("click", function(){
-    menumobile.classList.remove("show");
+cross.addEventListener("click", function () {
+  menumobile.classList.remove("show");
 });
 
-window.scrollTo(x-coordinate, y-coordinate)
-console.log()
+window.scrollTo(x - coordinate, y - coordinate);
+console.log();
